020de5277c9ce6361a34171bf0e1d662e8b4021c
rsgenack <your-github-email@example.com>
2025-04-12 00:18:32 -0400

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Archivo_Black } from 'next/font/google'
+import Script from 'next/script'
 
 const archivoBlack = Archivo_Black({
   weight: '400',
@@ -23,6 +24,16 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet" />
+        {/* Google Analytics */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-F49NS3SCY6" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F49NS3SCY6');
+          `}
+        </Script>
       </head>
       <body className={archivoBlack.className}>{children}</body>
     </html>
